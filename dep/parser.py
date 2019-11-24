e8c3a64be646aaff4cb5b7bd3d286fcdda137790
Marius Elvegård <marius.elvegard@me.com>
2019-11-24 19:05:51 +0100

    parser: added file header. Change from id_list[] to type_list[]

--- dep/parser.py
+++ dep/parser.py
@@ -1,13 +1,22 @@
+"""
+========================================================
+    Filename: parser.py
+    Author: Marius Elvegård
+    Description:
+
+
+========================================================
+"""
 
 _dep_keywords = ["library", "use", "context", "component"]
 _id_keywords  = ["entity", "package"]
 
-class Parser():
 
+
+class Parser():
 
     def __init__(self, tokens):
         self.tokens = tokens
-
 
     def get_dependency(self):
         dependency_list = []
@@ -28,8 +37,8 @@ class Parser():
 
         return dependency_list
 
-    def get_id(self):
-        id_list = []
+    def get_type(self):
+        type_list = []
         token_counter = 0
         while token_counter < len(self.tokens)-1:
             token_keyword   = self.tokens[token_counter][0]
@@ -37,13 +46,16 @@ class Parser():
 
             # Find VHDL keywords
             if token_keyword == "KEYWORD":
+
                 # Check if token is a entity keyword and save the first found
                 if token_word.lower() in _id_keywords:
-                    id_list.append([token_word.upper(),  self.tokens[token_counter+1][1]])
-
+                    type_list.append([token_word.upper(),  self.tokens[token_counter+1][1]])
                     # Done
                     break
 
             token_counter += 1
 
-        return id_list
+        if not( type_list ):
+            type_list.append(["OTHER", "OTHER"])
+
+        return type_list
