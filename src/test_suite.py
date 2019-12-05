b4a6d5d4a2867b439e8cc9a3b557d496880593c5
Marius Elvegård <marius.elvegard@me.com>
2019-12-05 20:29:13 +0100

    test_suite: added file and basic methods

---
+++ src/test_suite.py
@@ -0,0 +1,34 @@
+"""
+========================================================
+    Filename: test_suite.py
+    Author: Marius Elvegård
+    Description:
+
+
+========================================================
+"""
+
+
+class Test_suite:
+
+    def __init__(self):
+        self.tb_list = []
+        self.lib_list = []
+        self.testbench = None
+        self.compile_order_list = []
+
+    def add_testbench(self, testbench):
+        """
+        Set the testbench object for test suite
+        """
+        self.testbench = testbench
+
+    def get_testbench(self):
+        return self.testbench
+
+    
+    def set_compile_order(self, compile_order):
+        self.compile_order_list = compile_order
+    
+    def get_compile_order(self):
+        return self.compile_order_list
