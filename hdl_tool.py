0f478864dbae3576356e40c88504d489e86300c6
Marius Elvegård <marius.elvegard@me.com>
2019-12-06 10:14:41 +0100

    hdl_tool: added testing of several src objects

--- hdl_tool.py
+++ hdl_tool.py
@@ -9,16 +9,79 @@ from collection import Collection
 
 def main():
 
+
+    collection_list = []
+
+    # Create a collection and add to list
+    print("\nAdding bitvis_vip_uart")
     finder = Finder()
-    finder.add_files("test/*.vhd")
+    finder.add_files("test/bitvis_vip_uart/*.vhd")
     file_list = finder.get_files()
 
-
     collection = Collection()
-    collection.set_library("tb_lib")
+    collection.set_library("bitvis_vip_uart")
     collection.add_files(file_list)
     collection.organize()
     collection.list_compile_order()
+
+    collection_list.append(collection)
+
+    print("\nAdding bitvis_vip_sbi")
+    finder = Finder()
+    finder.add_files("test/bitvis_vip_sbi/*.vhd")
+    file_list = finder.get_files()
+
+    collection = Collection()
+    collection.set_library("bitvis_vip_sbi")
+    collection.add_files(file_list)
+    collection.organize()
+    collection.list_compile_order()
+
+    collection_list.append(collection)
+
+
+
+    print("\nAdding bitvis_vip_scoreboard")
+    finder = Finder()
+    finder.add_files("test/bitvis_vip_scoreboard/*.vhd")
+    file_list = finder.get_files()
+
+    collection = Collection()
+    collection.set_library("bitvis_vip_scoreboard")
+    collection.add_files(file_list)
+    collection.organize()
+    collection.list_compile_order()
+
+    collection_list.append(collection)
+
+
+    print("\nAdding uvvm_util")
+    finder = Finder()
+    finder.add_files("test/uvvm_util/*.vhd")
+    file_list = finder.get_files()
+
+    collection = Collection()
+    collection.set_library("uvvm_util")
+    collection.add_files(file_list)
+    collection.organize()
+    collection.list_compile_order()
+
+    collection_list.append(collection)
+
+
+    print("\nAdding uvvm_vvc_framework")
+    finder = Finder()
+    finder.add_files("test/uvvm_vvc_framework/src/*.vhd")
+    finder.add_files("test/uvvm_vvc_framework/src_target_dependent/*.vhd")
+    file_list = finder.get_files()
+
+    collection = Collection()
+    collection.set_library("uvvm_vvc_framework")
+    collection.add_files(file_list)
+    collection.organize()
+    collection.list_compile_order()
+
+    collection_list.append(collection)
 
 
 
