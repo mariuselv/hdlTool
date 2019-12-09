e20971a3a189e1c39b151d13e2c55051b566e812
Marius Elvegård <marius.elvegard@me.com>
2019-12-09 07:39:00 +0100

    test_suite: added basic methods

--- src/test_suite.py
+++ src/test_suite.py
@@ -14,33 +14,48 @@
 
 from compiler import Compiler
 
+
+
+
 class Test_suite:
 
     def __init__(self):
-        self.tb_list = []
-        self.lib_list = []
-        self.testbench = None
-        self.compile_order_list = []
-        self.compiler = Compiler()
+        self.tb_list                    = []
+        self.lib_list                   = []
+        self.testbench                  = None
+        self.compiler                   = Compiler()
+        self.organize_collection_list   = []
+
 
     def add_testbench(self, testbench):
         """
-        Set the testbench object for test suite
+        Set the testbench object for test suite.
+        Args:
+          testbench (str): testbench filename and path
         """
         self.testbench = testbench
 
     def get_testbench(self):
         return self.testbench
 
-    
-    def set_compile_order(self, compile_order):
-        self.compile_order_list = compile_order
-    
-    def get_compile_order(self):
-        return self.compile_order_list
+
+    def set_collection(self, organize_collection_list):
+        """
+        Add a collection to test suite
+
+        Args:
+          organize_collection_list (Collection object): a collection of VHDL object files.
+        """
+        self.organize_collection_list = organize_collection_list
+
+    def get_collection(self):
+        return self.organize_collection_list
+
 
     def compile(self):
+        #self.compiler.compile(self.organize_collection_list)
         pass
+
 
     def simulate(self):
         pass
