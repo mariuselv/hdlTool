140df0d2223f36038c8397d0d4169457c060bdf3
Marius Elvegård <marius.elvegard@me.com>
2020-08-07 20:33:02 +0200

    Collection/Finder: removed unused methods, moved Finder() class to collection.py

--- src/collection.py
+++ src/collection.py
@@ -11,7 +11,9 @@
 from pars import Parser
 from lexer import Lexer
 from vhd import VHD
-from finder import Finder
+from glob import glob
+import os
+
 
 
 class Collection:
@@ -24,47 +26,43 @@ class Collection:
         self.finder = Finder()
 
 
-    def set_library(self, library):
-        """
-        Set the compile library.
-        """
+    def set_library(self, library) -> None :
+        """ Set the compile library. """
         self.library = library
 
-    def get_library(self):
+    def get_library(self) -> str : 
         return self.library
 
 
-    def add_file(self, vhdl_file):
-        """
-        Adds a single file to builder.
-        """
-        self.finder.add_file(vhdl_file)
+    def add_file(self, vhdl_file) -> bool :
+        """ Adds a single file to builder. """
+        if not os.path.isfile(vhdl_file): return False
 
         if not vhdl_file in self.vhdl_source_file_list:
             self.vhdl_source_file_list.append(vhdl_file)
+            return True
+        return False
 
 
-    def add_files(self, vhdl_files):
+    def add_files(self, vhdl_files) -> bool :
         """
         Adds a list of files to builder.
         """
+        return_result = True
         self.finder.add_files(vhdl_files)
 
         for vhdl_file in self.finder.get_files():
-            self.add_file(vhdl_file)
-
-    def _remove_leftovers(self, sorted_list):
-
-        return sorted_list
-        
+            if not(self.add_file(vhdl_file)):
+                return_result = False
+        return return_result
 
 
-    def _swap(self, list, i, j):
+    def _swap(self, list, i, j) -> list:
         list[i], list[j] = list[j], list[i]
         return list
 
 
-    def _sort_compile_order(self, vhdl_file_list):
+    def _sort_compile_order(self, vhdl_file_list) -> list :
         sorted_list = vhdl_file_list.copy()
 
         # Slow sorting method, need N-1 loops
@@ -87,19 +85,31 @@ class Collection:
                         if check_idx < sort_idx:
                             sorted_list = self._swap(sorted_list, sort_idx, check_idx)
 
-        
-        self.vhdl_obj_list = self._remove_leftovers(sorted_list)
+        self.vhdl_obj_list = sorted_list
 
 
-    def organize_collection(self):    
-        tokens = []
+    def organize_collection(self):
+        """
+        With all files in collection do:
+        1. Create a vhdl_object() object
+        2. Tokenize file
+        3. Parse file
+        4. Read file dependencies
+        5. Read type of file
+        6. Set vhdl_object name, type and dependencies.
+        7. Add vhdl_object to vhdl_object_list
+        8. Detect vhdl_object generics
+
+        -> Final, sort vhdl_objects by dependency.
+        """
 
         for file_item in self.vhdl_source_file_list:
-
+            # Get new VHDL object
             vhdl_object = VHD(file_item)
 
+            # Read file content
             with open(file_item, 'r') as file:
-                content = file.read() 
+                content = file.read()
 
             # Lexer
             lex = Lexer(content)
@@ -112,7 +122,7 @@ class Collection:
             # Get the type of vhdl object
             vhdl_type    = parser.get_type()
 
-            # vhdl object
+            # VHDL object
             # Set the name of vhdl object
             vhdl_object.set_name(vhdl_type)
             # Set the type of vhdl object
@@ -125,21 +135,17 @@ class Collection:
 
             vhdl_object.get_generics(tokens)
 
+        # Organize VHDL objects by internal dependeny
         self._sort_compile_order(self.vhdl_obj_list)
 
 
-    def get_local_dep_list(self):
+    def get_compile_order(self) -> list :
+        """ Return list of library internal dependencies. """
         return self.vhdl_obj_list
 
-    def list_compile_order(self):
-        print("\n================================ %s ================================" %(self.get_library()))
-        for idx, item in enumerate(self.vhdl_obj_list):
-            print("[%i] File: %s (%s: %s)" %(idx+1, item.get_filename(), item.get_type(), item.get_name()))
 
-    def get_compile_order(self):
-        return self.vhdl_obj_list
-
-    def get_external_dependency(self):
+    def get_external_dependency(self) -> list :
+        """ Return list of external libraries dependencies. """
         ret_list = []
         for obj in self.vhdl_obj_list:
             libs = obj.get_lib_dep()
@@ -148,3 +154,38 @@ class Collection:
                     ret_list.append(lib)
 
         return ret_list
+
+
+    def list_compile_order(self) -> None :
+        print("\n================================ %s ================================" %(self.get_library()))
+        for idx, item in enumerate(self.vhdl_obj_list):
+            print("[%i] File: %s (%s: %s)" %(idx+1, item.get_filename(), item.get_type(), item.get_name()))
+
+
+
+
+
+
+
+class Finder():
+    """
+    Use for file search and adding files.
+    """
+
+    def __init__(self):
+        self.file_list = []
+
+    def add_file(self, filename) -> None :
+        if not filename in self.file_list:
+            self.file_list.append(filename)
+
+    def add_files(self, pattern) -> None:
+        files = glob(pattern)
+        for item in files:
+            self.add_file(item)
+
+    def get_files(self) -> list :
+        return self.file_list
+
+    def flush(self) -> None :
+        self.file_list = []
