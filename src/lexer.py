3dfcd4b399b2a3e83e6d84b35f3c3fce38c2ee3e
Marius Elvegård <marius.elvegard@me.com>
2020-08-10 19:24:20 +0200

    Test: added lexer line and word count testing

--- src/lexer.py
+++ src/lexer.py
@@ -34,9 +34,14 @@ class Lexer(object):
         return re.findall(r'\S+|\n', source_code)
 
     def tokenize(cls, source_code, tokens) -> list :
-        """ Create tokens list """
+        """ Create tokens list from source code string """
+
         # Word index counter
         source_index = 0
+
+        # Counters
+        line_number = 1
+        word_number = 1
 
         # Set to True when parsing inside a string
         in_string = False
@@ -51,85 +56,91 @@ class Lexer(object):
 
             # New line detected: reset
             if '\n' == word:
-                tokens.append(["NEW_LINE", '\n'])
+                tokens.append(["NEW_LINE", '\n', line_number, word_number])
                 in_comment = False
                 in_string = False
 
 
             # Check if this is the start of a comment line
             if (word[0:2] == "--") and not(in_string) and not(in_comment):
-                tokens.append(["SYMBOL", word[0:2]])
+                tokens.append(["SYMBOL", word[0:2], line_number, word_number])
                 in_comment = True
 
                 # Check if this is the end of the comment line
                 if word[len(word)-2] == '\n':
                     in_comment = False
-                    tokens.append(["COMMENT", word[:len(word)-2]])
-                    tokens.append(["NEW_LINE", '\n'])
+                    tokens.append(["COMMENT", word[:len(word)-2], line_number, word_number])
+                    tokens.append(["NEW_LINE", '\n', line_number, word_number])
                 # Check if comment is right after comment identifier
                 elif len(word) > 2:
-                    tokens.append(["COMMENT", word[2:]])
+                    tokens.append(["COMMENT", word[2:], line_number, word_number])
 
             # This is an active comment line
             elif in_comment:
-                tokens.append(["COMMENT", word])
+                tokens.append(["COMMENT", word, line_number, word_number])
                 if word[len(word)-1] == '\n':
                     in_comment = False
-                    tokens.append(["NEW_LINE", '\n'])
+                    tokens.append(["NEW_LINE", '\n', line_number, word_number])
 
 
             # Detect code strings
             elif (word in hdlTool_pkg.string) and not(in_string):
                 in_string = True
-                tokens.append(["SYMBOL", word])
+                tokens.append(["SYMBOL", word, line_number, word_number])
             elif (word in hdlTool_pkg.string) and in_string:
                 in_string = False
-                tokens.append(["SYMBOL", word])
+                tokens.append(["SYMBOL", word, line_number, word_number])
             elif in_string:
-                tokens.append(["STRING", word])
+                tokens.append(["STRING", word, line_number, word_number])
             # Need checks for strings of type: "foo", not only: "foo  and foo"
 
 
             elif not(in_string) and not(in_comment):
 
+                # Remove statement ending ';' from word, will be added later
                 if word[len(word)-1] == ";":
                     statement_end = True
                     word = word[:len(word)-1]
 
                 if word.lower() in hdlTool_pkg.keywords:
-                    tokens.append(["KEYWORD", word])
+                    tokens.append(["KEYWORD", word, line_number, word_number])
 
                 elif word.lower() in hdlTool_pkg.commons:
-                    tokens.append(["COMMON_KEYWORD", word])
+                    tokens.append(["COMMON_KEYWORD", word, line_number, word_number])
 
                 elif word in hdlTool_pkg.assignment:
-                    tokens.append(["ASSIGNMENT", word])
+                    tokens.append(["ASSIGNMENT", word, line_number, word_number])
 
                 elif word in hdlTool_pkg.operator:
-                    tokens.append(["OPERATOR", word])
+                    tokens.append(["OPERATOR", word, line_number, word_number])
 
                 elif word in hdlTool_pkg.symbols:
-                    tokens.append(["SYMBOL", word])
+                    tokens.append(["SYMBOL", word, line_number, word_number])
 
                 elif re.match('[0-9]', word):
-                    tokens.append(["INTEGER", word])
+                    tokens.append(["INTEGER", word, line_number, word_number])
 
                 elif re.match('[a-z]', word.lower()):
-                    tokens.append(["IDENTIFIER", word])
+                    tokens.append(["IDENTIFIER", word, line_number, word_number])
 
                 elif word in hdlTool_pkg.comments:
-                    tokens.append(["COMMENT", word])
+                    tokens.append(["COMMENT", word, line_number, word_number])
 
                 elif word in hdlTool_pkg.new_line:
-                    tokens.append(["NEW_LINE", "\n"])
+                    tokens.append(["NEW_LINE", "\n", line_number, word_number])
 
                 else:
                     if not('\n') in word:
-                        tokens.append(["UNKNOWN", word])
+                        tokens.append(["UNKNOWN", word, line_number, word_number])
 
                 if statement_end:
-                   tokens.append(["STATEMENT_END", ";"])
+                   tokens.append(["STATEMENT_END", ";", line_number, word_number])
 
+            if tokens[len(tokens)-1][0] == "NEW_LINE":
+                line_number += 1
+                word_number = 1
+            else:
+                word_number += 1
 
             # Increment word index counter
             source_index += 1
--- test/test_lexer.py
+++ test/test_lexer.py
@@ -7,7 +7,7 @@ from lexer import Lexer
 
 def setup_function():
     global lexer
-    lexer = Lexer("this is the source code")
+    lexer = Lexer("this is the source code\nthis is the second line\nthis is the third line\n")
 
 def tear_down_function():
     pass
@@ -28,3 +28,21 @@ def test_incomplete_string_double_quote():
 def test_incomplete_string_single_quote():
     result = lexer._complete_string("'this is an incomplete string", "\'")
     assert result == False, "Expecting incomplete single quoted string"
+
+def test_word_counters():
+    result = lexer.get_tokens()
+
+    assert result[0][2] == 1, "Expecting line 1"
+    assert result[0][3] == 1, "Expecting word 1"
+
+    assert result[1][2] == 1, "Expecting line 1"
+    assert result[1][3] == 2, "Expecting word 2"
+
+    assert result[4][2] == 1, "Expecting line 1"
+    assert result[4][3] == 5, "Expecting word 5"
+
+    assert result[5][2] == 1, "Expecting line 1"
+    assert result[5][3] == 6, "Expecting word 6"
+
+    assert result[6][2] == 2, "Expecting line 2"
+    assert result[6][3] == 1, "Expecting word 1"
